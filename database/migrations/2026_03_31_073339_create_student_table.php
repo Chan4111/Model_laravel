25fe0925d700fce901c47d01c28588ef0d61b3df
Chandan kumar thakur <tchandankumar2004@gmail.com>
2026-03-31 14:38:36 +0530

    Operation of Foreign key and primary key in laravel

--- database/migrations/2026_03_31_073339_create_student_table.php
+++ database/migrations/2026_03_31_073339_create_student_table.php
@@ -11,9 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('city', function (Blueprint $table) {
+        Schema::create('students', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name',30);
+            $table->string('email',40)->nullable()->unique();
         });
     }
 
@@ -22,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('city');
+        Schema::dropIfExists('students');
     }
 };
--- database/migrations/2026_03_31_085948_updated_libraries_table.php
+++ database/migrations/2026_03_31_085948_updated_libraries_table.php
@@ -11,10 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('user', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('city');
+        Schema::table('libraries', function (Blueprint $table) {
+            $table->dropForeign(['student_id']);
         });
     }
 
@@ -23,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('user');
+        Schema::table('libraries', function (Blueprint $table) {
+            //
+        });
     }
 };
